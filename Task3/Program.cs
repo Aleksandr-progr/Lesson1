72766e73ce892a031812d8c9d2feff7cc2e10a56
Alekandr-prog <a.a.sergeev1978@yandex.ru>
2023-12-25 01:15:16 +0300

    lesson :task3 решение домашнего зд

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Заданые целые чичла
+int a = 3;
+int b = 5;
+int c = 2;
+int d = 7;
+// Вычесление значения по формуле
+double result = (a * b) / (double) (c + d);
+
+// Вывод результата на экран 
+Console.WriteLine("Рузультат:" + result);
