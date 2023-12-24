fafa9c67409b4cb6e9332a4e2cdbb0b3c8763d32
Alekandr-prog <a.a.sergeev1978@yandex.ru>
2023-12-25 00:39:00 +0300

    add lessoni: task2

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61e3944b2af649d5cb2a675b191298443917d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ee2f0ab240b16297575aec379decb24f3eb1254")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
