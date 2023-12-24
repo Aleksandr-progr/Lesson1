d6c60a99da3369673c35c06ff7caef857aaa65a1
Alekandr-prog <a.a.sergeev1978@yandex.ru>
2023-12-25 02:03:11 +0300

    lesson :task5  сравнение больше меньше

---
+++ Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 43;
+int b = 12;
+int c = 23;
+int d = 3;
+int e = 19;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
