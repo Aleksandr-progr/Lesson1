10a902b29c2b82df6e65d71d72b220f56635aa18
Alekandr-prog <a.a.sergeev1978@yandex.ru>
2023-12-30 02:45:24 +0300

    Решение домашней задачи №2

--- Homemade2/Program.cs
+++ Homemade2/Program.cs
@@ -1,32 +1,24 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
-// Вводим координаты точки
-Console.Write("Введите координату X: ");
-double x = double.Parse(Console.ReadLine());
-Console.Write("Введите координату Y: ");
-double y = double.Parse(Console.ReadLine());
+using System;
 
-// Проверяем, что X и Y не равны нулю
-if (x == 0 || y == 0)
-{
-    Console.WriteLine("X и Y не должны быть равны нулю");
-}
-else
-{
-    // Определяем номер четверти плоскости
-    if (x > 0 && y > 0)
-    {
-        Console.WriteLine("Точка находится в первой четверти");
-    }
-    else if (x < 0 && y > 0)
-    {
-        Console.WriteLine("Точка находится во второй четверти");
-    }
-    else if (x < 0 && y < 0)
-    {
-        Console.WriteLine("Точка находится в третьей четверти");
-    }
-    else
-    {
-        Console.WriteLine("Точка находится в четвертой четверти");
+class Program {
+    static void Main() {
+        Console.Write("Введите координаты точки X и Y через пробел: ");
+        string[] coordinates = Console.ReadLine().Split(' ');
+        int x = int.Parse(coordinates[0]); // Используйте int.Parse вместо Convert.ToInt32 для простоты
+        int y = int.Parse(coordinates[1]);
+        if (x == 0 || y == 0) { // Проверьте сначала, что точка не находится на оси координат
+            Console.WriteLine("Точка находится на оси координат");
+        }
+        else {
+            int quadrant = 0; // Объявите переменную для хранения номера четверти
+            if (x > 0) {
+                quadrant = (y > 0) ? 1 : 4; // Используйте тернарный оператор для присваивания значения
+            }
+            else {
+                quadrant = (y > 0) ? 2 : 3;
+            }
+            Console.WriteLine(quadrant); // Выведите значение переменной
+        }
     }
 }
